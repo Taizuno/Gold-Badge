e6ecdb5d791f4b25c8bc57481115f87b9360ebaf
Braeden <lightning9501@gmail.com>
2022-06-30 14:43:02 -0400

    Challenge4

---
+++ Challenge_3/Test/Badge_repo_test.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XUnit;
+
+namespace Challenge_3.Test
+{
+    public class Badge_repo_test
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Challenge_3/src/poco/Badge.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_3.src.poco
+{
+    public class Badge
+    {
+        
+    }
+}
