2e9b935f0e635caad53bbcc102201e77a40e4a8f
Braeden McNealy <lightning9501@gmail.com>
2022-06-30 17:27:39 -0400

    Lastminute

--- Challenge_3/src/poco/Badge.cs
+++ Challenge_3/src/poco/Badge.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenge_3.src.poco
-{
+//namespace Challenge_3.src.poco
+
     public class Badge
     {
-        
+        Badge(){}
+
+        public int BadgeID{get; set;}
+        public List<string> Access{get;set;}
     }
-}
--- Challenge_3/src/repo/Badge_Repo.cs
+++ Challenge_3/src/repo/Badge_Repo.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenge_3.src.repo
-{
+//namespace Challenge_3.src.repo
     public class Badge_Repo
     {
-        
+        private readonly Dictionary<int, List<string>> _bdic = new Dictionary<int, List<string>>();
+        private int _count = 0;
+        public bool AddBadge(Badge b)
+        {
+            if(Badge is null)
+            {
+                return false;
+            }
+
+            _count++;
+            b.BadgeID = _count;
+            _bdic.Add(b);
+            return true;
+        }
+        public Dictionary<int, List<string>> ViewBadges()
+        {
+            return _bdic;
+        }
+        public Badge GetBadgeByID(int id)
+        {
+            foreach(KeyValuePair<int,List<string>> badge in _bdic)
+            {
+                if(badge.Key == id)
+                {
+                    return badge;
+                }
+            }
+        }
     }
-}
