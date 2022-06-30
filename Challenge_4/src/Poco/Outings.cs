4450ece85bafdabf7ae5ede73676c8d4244ac0a3
Braeden <lightning9501@gmail.com>
2022-06-30 14:46:03 -0400

    Challenge3

---
+++ Challenge_4/src/Poco/Outings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_4.src.Poco
+{
+    public class Outings
+    {
+        
+    }
+}
