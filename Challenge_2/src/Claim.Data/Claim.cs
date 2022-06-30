5bfacd7eca68bea6d149e6ea6925454e958780a7
Braeden McNealy <lightning9501@gmail.com>
2022-06-30 16:33:06 -0400

    Challenge2src

--- Challenge_2/src/Claim.Data/Claim.cs
+++ Challenge_2/src/Claim.Data/Claim.cs
@@ -7,5 +7,13 @@ using System.Threading.Tasks;
 
     public class Claim
     {
-        
+        Claim(){}
+
+        public int ClaimID{get;set;}
+        public string ClaimType{get;set;}
+        public string Description{get;set;}
+        public double ClaimAmount{get;set;}
+        public DateOnly DateOfIncident{get;set;}
+        public DateOnly DateOfClaim{get;set;}
+        public bool isValid{get;set;}
     }
--- Challenge_2/src/Claim.Repository/Class_Repo.cs
+++ Challenge_2/src/Claim.Repository/Class_Repo.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenge_2.src.Claim.Repository
-{
-    public class Class_Repo
+//namespace Challenge_2.src.Claim.Repository
+
+    public class Claim_Repo
     {
-        
+        private readonly List<Claim> _crepo = new List<Claim>();
+        private int _count;
+        public void AddClaim(Claim claim)
+        {
+            if(claim != null)
+            {
+                _count++;
+                claim.ClaimID = _count;
+                _crepo.Add(claim);
+            }
+        }
+        public List<Claim> ViewAll()
+        {
+            return _crepo;
+        }
+        public bool RemoveClaim(Claim claim)
+        {
+            if(claim != null)
+            {
+                bool success = _crepo.Remove(claim);
+                if(success)
+                {return true;}
+            }
+            return false;
+        }
     }
-}
