9bd97ccdf7373b00272b5961bed5a37d4bc3333e
Braeden <lightning9501@gmail.com>
2022-06-30 13:28:21 -0400

    Challenge2Outline

---
+++ Challenge_2/src/Claim.Data/Claim.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_2.src
+{
+    public class Claim
+    {
+        
+    }
+}
---
+++ Challenge_2/src/Claim.Repository/Class_Repo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_2.src.Claim.Repository
+{
+    public class Class_Repo
+    {
+        
+    }
+}
