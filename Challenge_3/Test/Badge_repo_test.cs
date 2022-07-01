7cfe6d6a50ffaaa3e6451ecd16f2870b60715cb8
Braeden McNealy <lightning9501@gmail.com>
2022-07-01 17:51:41 -0400

    extra

--- Challenge_3/Test/Badge_repo_test.cs
+++ Challenge_3/Test/Badge_repo_test.cs
@@ -2,16 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using XUnit;
+using Xunit;
 
-namespace Challenge_3.Test
-{
+//namespace Challenge_3.Test
+
     public class Badge_repo_test
     {
+        private readonly Badge_Repo _bd = new Badge_Repo();
+
+        private Badge _badge = new Badge();
         [Fact]
-        public void Test1()
+        public void AddBadge_True()
         {
-            Assert.True(true);
+            var badge = new Badge();
+            bool success = _bd.AddBadge(badge);
+            Assert.True(success);
+        }
+        [Fact]
+        public void GetBadgeByID_True()
+        {
+            _bd.AddBadge(_bd);
+            var badge = _bd.GetBadgeByID(1);
+            int actual = badge.BadgeID;
+            int expected = 1;
+            Assert.Equals(expected, actual);
+        }
+        [Fact]
+        public void ViewBadges_List()
+        {
+            
         }
     }
-}
